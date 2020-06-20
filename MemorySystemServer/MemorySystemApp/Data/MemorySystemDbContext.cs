1a40bb556d0fef1c4dd4370ce888c1918b2e7ede
HrBozhidarov <hristo_bozhidarov@abv.bg>
2020-06-20 18:49:47 +0300

    Add db sets.

--- MemorySystemServer/MemorySystemApp/Data/MemorySystemDbContext.cs
+++ MemorySystemServer/MemorySystemApp/Data/MemorySystemDbContext.cs
@@ -11,5 +11,28 @@
             : base(options)
         {
         }
+
+        public DbSet<Picture> Pictures { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<CategoryPicture> CategoryPictures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<CategoryPicture>()
+                .HasOne(e => e.Category)
+                .WithMany(e => e.CategoryPictures)
+                .HasForeignKey(e => e.CategoryId);
+
+            builder.Entity<CategoryPicture>()
+                .HasOne(e => e.Picture)
+                .WithMany(e => e.CategoryPictures)
+                .HasForeignKey(e => e.PictureId);
+
+            base.OnModelCreating(builder);
+        }
     }
 }
