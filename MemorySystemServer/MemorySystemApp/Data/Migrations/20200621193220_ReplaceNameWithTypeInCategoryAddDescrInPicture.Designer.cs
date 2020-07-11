66dd2c7c87c7319abd42556bc1c27d7635d9aa9f
HrBozhidarov <hristo_bozhidarov@abv.bg>
2020-07-11 13:51:49 +0300

    Add migration.

--- MemorySystemServer/MemorySystemApp/Data/Migrations/20200621193220_ReplaceNameWithTypeInCategoryAddDescrInPicture.Designer.cs
+++ MemorySystemServer/MemorySystemApp/Data/Migrations/20200621193220_ReplaceNameWithTypeInCategoryAddDescrInPicture.Designer.cs
@@ -4,14 +4,16 @@ using MemorySystemApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MemorySystemApp.Data.Migrations
 {
     [DbContext(typeof(MemorySystemDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200621193220_ReplaceNameWithTypeInCategoryAddDescrInPicture")]
+    partial class ReplaceNameWithTypeInCategoryAddDescrInPicture
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MemorySystemServer/MemorySystemApp/Data/Migrations/20200621193220_ReplaceNameWithTypeInCategoryAddDescrInPicture.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MemorySystemApp.Data.Migrations
+{
+    public partial class ReplaceNameWithTypeInCategoryAddDescrInPicture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Categories");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Pictures",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Categories",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Pictures");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Categories");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
